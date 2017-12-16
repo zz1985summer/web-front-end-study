b2bedfbcacd084b0bcf63902a20d88c95f8cb043
zz1985summer <zz1985summer@gmail.com>
2017-12-16 11:26:16 +0800

    login animation

--- pm/js/login.js
+++ pm/js/login.js
@@ -1,3 +1,17 @@
+/*----add welcome msg animation----*/
+$(document).ready(function(){
+    var message='Welcome...';
+    var i=0;
+    function addWord() {
+        if (i < message.length){
+            $('#welcome_words').text($('#welcome_words').text()+message.charAt(i));//message.charAt(i);
+            i++;
+        }
+         setTimeout(addWord,300);
+    }
+    addWord();
+});
+
 /*----valide null and pop up tp msg---*/
 $('#logBtn').bind('click',function(){
 	if($('#username').val()==="" && $('#password').val()===""){
@@ -19,8 +33,12 @@ $('#logBtn').bind('click',function(){
 
 /*---destroy tp---*/
 $('#username').bind('mouseover',function(){
-	$('#username').tooltip('destroy');
-	$('#password').tooltip('destroy');	
-	
+	if($('#username').val()!==""){
+        $('#username').tooltip('destroy');
+	}
 });
-
+$('#password').bind('mouseover',function(){
+    if($('#password').val()!==""){
+        $('#password').tooltip('destroy');
+	}
+});
